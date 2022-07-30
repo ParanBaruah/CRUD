749884725fd4277ee6926376b19da1b3e190bc35
ParanBaruah <paranjyotibaruah@gmail.com>
2022-07-30 21:42:20 +0530

    commit

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -6,6 +6,36 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            @if (session()->has('name'))
+                {{session()->get('name')}}
+            @else
+                Guest
+            @endif
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('/companies')}}">Companies</a>
+                </li>
+                
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
 <div class="container mt-2">
 <div class="row">
 <div class="col-lg-12 margin-tb">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyCRUDController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('companies', CompanyCRUDController::class);
+Route::get('get-all-session',function(){
+    $session = session()->all();
+    p($session);
+});
+Route::get('set-session',function(Request $request){
+    $request->session()->put('name','Amazon');
+    $request->session()->put('email','amazon@amazon.com');
+    return redirect('get-all-session');
+});
+Route::get('destroy-session',function(){
+    session()->forget('name');
+    session()->forget('email');
+    return redirect('get-all-session');
+});
